9cb1cc3263a6aaf837c2d3d67477c81998363144
jerickrupertisip <jerickrupertisip@gmail.com>
2025-10-16 17:36:38 +0800

    WIP: add function helpers

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,4 +5,28 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   User? get currentUser => _auth.currentUser;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  Future<String?> signIn({
+    required String email,
+    required String pass,
+}) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+          email: email.trim(),
+          password: pass
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return _handleAuthException(e);
+    }
+  }
+
+  String _handleAuthException(FirebaseAuthException e) {
+    switch (e.code) {
+      case "user-not-found":
+        return "N";
+    }
+  }
 }
